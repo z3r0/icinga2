8d33e9936d6953529115f6f148611a2236645803
Jean Flach <jean-marcel.flach@icinga.com>
2018-04-09 13:00:12 +0200

    Add missing include

--- lib/base/process.cpp
+++ lib/base/process.cpp
@@ -17,6 +17,7 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.             *
  ******************************************************************************/
 
+#include "base/application.hpp"
 #include "base/process.hpp"
 #include "base/exception.hpp"
 #include "base/convert.hpp"
